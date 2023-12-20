5feff87387df18f804ad3d97a53641a6fad98647
Deepsn <75620590+Deepsn@users.noreply.github.com>
2023-12-19 23:43:19 -0300

    ci: sync place types

--- src/services.d.ts
+++ src/services.d.ts
@@ -1,9 +1,14 @@
 interface ReplicatedStorage {
 	Shared: Folder & {
+		components: Folder & {
+			drop: ModuleScript;
+			model: ModuleScript;
+		};
 		constants: Folder & {
 			core: ModuleScript;
 		};
 		matter: Folder & {
+			component: ModuleScript;
 			start: ModuleScript;
 		};
 		systems: Folder;
@@ -12,13 +17,23 @@ interface ReplicatedStorage {
 		Promise: ModuleScript;
 		RuntimeLib: ModuleScript;
 	};
+	Drops: Folder & {
+		Part: Model & {
+			Part: Part;
+		};
+	};
 }
 
 interface ServerScriptService {
 	TS: Folder & {
 		runtime: Script;
 		systems: Folder & {
-			"test-system": ModuleScript;
+			"spawn-drop": ModuleScript;
 		};
 	};
+}
+
+interface Workspace {
+	Baseplate: Part;
+	readonly Terrain: Terrain;
 }
